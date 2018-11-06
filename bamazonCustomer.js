a99dcd8854ae383d7735207e7b81b4e28a3ec24e
Vita Chen <39967407+vita-wanchenchen@users.noreply.github.com>
2018-11-05 23:48:03 -0500

    bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,9 +18,97 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    startShopping();
   });
 
-  function startShopping() {
-      
-  };
+// Display all of available items.
+function displayInventory() {
+	connection.query('SELECT * FROM products', function(err, res){
+		if (err) throw err;
+        console.log('=============================What would you like to buy?==============================');
+        
+        for(var i = 0; i<res.length;i++){
+            console.log("ID: " + res[i].item_id + " | " + 
+                        "Product: " + res[i].product_name + " | " + 
+                        "Department: " + res[i].department_name + " | " + 
+                        "Price: " + res[i].price + " | " + 
+                        "QTY: " + res[i].stock_quantity);
+            console.log('======================================================================================');            
+          };
+          buyItem();
+        });
+};
+
+// Prompt for whaich item user would like to buy and how many of the item.
+function buyItem() {
+    inquirer
+      .prompt([
+          {
+            name: 'itemId',
+            type: 'input',
+            message: 'Which item would you like to buy? (Please put in the Item ID)',
+            validate: function(value) {
+                if (isNaN(value) === false && parseInt(value) > 0 && parseInt(value) <= 10) {
+                  return true;
+                }
+                return 'Please put in valid Item ID.';
+              }
+          },
+          {
+            name: 'quantity',
+            type: 'input',
+            message: 'How many would you like to buy?',
+            validate: function(value) {
+                if (isNaN(value) === false && parseInt(value) > 0) {
+                  return true;
+                }
+                return "Please enter valid quantity.";
+              }
+          }
+      ]).then(function(answer){
+          var query = 'SELECT * FROM products WHERE item_id = ?';
+            connection.query(query, [answer.itemId], function(err, res){
+            
+            // Checking if have enough inventory.
+            if(answer.quantity > res[0].stock_quantity){
+            console.log('Insufficient Quantity');
+            console.log('This order has been cancelled');
+            console.log('');
+            newOrder();
+            }
+            else{
+                totalPrice = res[0].price * answer.quantity;
+                console.log('Your Total is $' + totalPrice);
+                console.log('Thank you for your order and enjoy!');
+                console.log('');
+
+                //update products table
+                connection.query('UPDATE products SET ? Where ?', [{
+                    stock_quantity: res[0].stock_quantity - answer.quantity
+                },{
+                    item_id: answer.itemId
+                }], function(err, res){});
+                newOrder();
+            }
+        })
+    
+    }, function(err, res){})
+};
+
+//Allows the user to place a new order or end the connection
+function newOrder(){
+	inquirer.prompt([{
+		type: 'confirm',
+		name: 'choice',
+		message: 'Would you like to place another order?'
+	}]).then(function(answer){
+		if(answer.choice){
+			buyItem();
+		}
+		else{
+			console.log('Thank you for shopping at Bamazon!');
+			connection.end();
+		}
+	})
+};
+
+displayInventory();
--- seeds.sql
+++ seeds.sql
@@ -3,13 +3,13 @@ USE bamazon_DB
 
 /*Populate 10 different products*/
 INSERT INTO products (product_name, department_name, price,stock_quantity)
-VALUES ("toy mouse", "pet", 3.5, 150), 
-       ("shampoo", "personal care", 4.5, 250),
-       ("bluetooth headset", "electronics", 25, 50),
-       ("24\" monitor", "electronics", 220.75, 15),
-       ("handbag", "appeal", 18, 100),
-       ("body lotion", "personal care", 5, 5),
-       ("polo shirt", "appeal", 7.5, 300),
-       ("milky bone", "pet", 10, 25),
-       ("mug", "dishes", 3, 1200),
-       ("round plate", "dishes", 4.5, 1);
+VALUES ("Toy Mouse", "Pet", 3.5, 150), 
+       ("Shampoo", "Personal Care", 4.5, 250),
+       ("Bluetooth Headset", "Electronics", 25, 50),
+       ("24\" Monitor", "Electronics", 220.75, 15),
+       ("Handbag", "Appeal", 18, 100),
+       ("Body Lotion", "Personal Care", 5, 5),
+       ("Polo Shirt", "Appeal", 7.5, 300),
+       ("Milky Bone", "Pet", 10, 25),
+       ("Mug", "Dishes", 3, 1200),
+       ("Round Plate", "Dishes", 4.5, 1);
